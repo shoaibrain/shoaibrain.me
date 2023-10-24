00994f660b71d82d86d1c0689c35ce992d3a6926
Shoaib Rain <rainshoaib2013@outlook.com>
2023-10-24 18:38:17 -0500

    add copyright

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -7,7 +7,10 @@ import { ModeToggle } from "./mode-toggle"
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <div className="flex justify-end mr-8 p-4">
+      <div className="flex justify-around mr-8 p-4">
+        <p className="text-center text-sm leading-loose md:text-left">
+          Shoaib Rain &copy; {new Date().getFullYear()}
+        </p>
         <ModeToggle />
       </div>
     </footer>
