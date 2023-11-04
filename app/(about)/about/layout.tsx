5129709e7bdc8175d7911b5a895e7c8148a98f8b
Shoaib Rain <rainshoaib2013@outlook.com>
2023-11-04 10:43:40 -0500

    add dotted-bg -beta

--- app/(about)/about/layout.tsx
+++ app/(about)/about/layout.tsx
@@ -1,3 +1,4 @@
+import Background from "@/components/background"
 import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
 import { marketingConfig } from "@/config/site"
@@ -10,6 +11,7 @@ export default async function AboutLayout({
   children,
 }: AboutLayoutProps) {
   return (
+    <Background>
     <div className="flex min-h-screen flex-col">
       <header className="container z-40 bg-background">
         <div className="flex h-20 items-center justify-between py-6">
@@ -19,5 +21,6 @@ export default async function AboutLayout({
       <main className="flex-1">{children}</main>
       <SiteFooter className="border-t" />
     </div>
+    </Background>
   )
 }
--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import Background from "@/components/background"
 import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
 import { marketingConfig } from "@/config/site"
@@ -10,6 +11,7 @@ export default async function HomeLayout({
   children,
 }: HomeLayoutProps) {
   return (
+    <Background>
     <div className="flex min-h-screen flex-col">
       <header className="container z-40 bg-background">
         <div className="flex h-20 items-center border-t justify-between py-6">
@@ -19,5 +21,6 @@ export default async function HomeLayout({
       <main className="flex-1">{children}</main>
       <SiteFooter className="border-t" />
     </div>
+    </Background>
   )
 }
